0f9b6292d7cbe3e1db0dc79b3b768fbfe7b4ec4d
jocoonopa <jocoonopa@gmail.com>
2018-02-10 03:14:31 +0800

    Fixed response misspelled error, and change version number

--- src/LINEBot.php
+++ src/LINEBot.php
@@ -3,6 +3,7 @@
 namespace jocoonopa\LINEBot;
 
 use LINE\LINEBot as OrgLINEBot;
+use LINE\LINEBot\MessageBuilder;
 use LINE\LINEBot\Response;
 use Log;
 
@@ -20,6 +21,11 @@ class LINEBot
      */
     protected $accessToken = null;
 
+    /**
+     * @var LINE\LINEBot\MessageBuilder
+     */
+    protected $msgBuilder = null;
+
     public function __construct(OrgLINEBot $bot = null)
     {
         $this->setBot($bot);
@@ -36,11 +42,30 @@ class LINEBot
     {
         $response = call_user_func_array([$this->getBot(), $name], $arguments);
 
-        if (config('linebot.is_debug') && $resopnse instanceof Response) {
-            Log::debug($response->getJSONDecodedBody());
+        if (!config('linebot.is_debug') || !($response instanceof Response)) {
+            return $response;
         }
 
+        foreach ($arguments as $argument) {
+            if ($argument instanceof MessageBuilder) {
+                $this->msgBuilder = $argument;
+
+                break;
+            }
+        }
+
+        Log::debug($this->getDebugContent(), [
+            'content' => $response->getJSONDecodedBody(),
+            'status' => $response->getHTTPStatus(),
+            'headers' => $response->getHeaders(),
+        ]);
+
         return $response;
+    }
+
+    protected function getDebugContent()
+    {
+        return is_object($this->msgBuilder) ? get_class($this->msgBuilder) : 'null';
     }
 
     /**
